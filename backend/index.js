259ccdb6568f6fb2976eaa7180114bcbb20568fe
PikbeeOnCode <pikbeekrish@gmail.com>
2026-02-18 16:22:58 +0545

    fix cors for production

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import dotenv from "dotenv";
+import cors from "cors";
 
 // utilites
-import connectDB from "./config/db.js";  // added .js here
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
@@ -18,23 +20,27 @@ connectDB();
 
 const app = express();
 
+app.use(cors({
+  origin: "https://krish-ecommerce-mmg2.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/users',userRoutes);
-app.use('/api/category',categoryRoutes);
-app.use('/api/Products',productRoutes);
-app.use('/api/upload',uploadRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/Products', productRoutes);
+app.use('/api/upload', uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-
 const __dirname = path.resolve();
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.listen(PORT, () => {
   console.log("Port 3000 is listening!");
