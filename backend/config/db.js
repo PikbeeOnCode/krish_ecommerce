3fcfe8fd38ae7eb6d2381caac7a861fb6e1224ef
PikbeeOnCode <pikbeekrish@gmail.com>
2026-02-18 13:11:24 +0545

    add start script

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI_ONLINE);
         console.log('sucessfully connected to mongoDB')
     } catch (error) {
         console.error(`error ${error.message}`);
